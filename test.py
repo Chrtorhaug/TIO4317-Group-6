f80346a6cfc26a6a13414e5ebcc4ec46829098ae
Chrtorhaug <Chrtorhaug@gmail.com>
2025-03-27 14:34:26 +0100

    Added general structure. Made plots for major parts of the project

--- test.py
+++ test.py
@@ -19,43 +19,45 @@ rut['Date'] = pd.to_datetime(rut['Date'])
 rut["Return"] = rut["Close"].pct_change()
 rut = rut.dropna(subset={"Return"})
 
-model = sm.ARIMA(spx['Return'], order=(1, 0, 1)).fit()
-resid = model.resid
+def plot_annualized_volatility(index: pd.DataFrame, model_type: str, symmetry: int):
+    model = sm.ARIMA(index['Return'], order=(1, 0, 1)).fit()
+    resid = model.resid
+    
+    p_range = range(1, 5)
+    q_range = range(1, 5)
+    results = []
 
-p_range = range(1, 5)
-q_range = range(1, 5)
-results = []
+    for p in p_range:
+        for q in q_range:
+            try:
+                model = arch_model(resid, vol=model_type, p=p, o=symmetry, q=q, dist='normal', rescale=False)
+                fitted_model = model.fit(disp='off')
+                results.append({'p': p, 'q': q, 'AIC': fitted_model.aic, 'BIC': fitted_model.bic})
+            except Exception as e:
+                print(f"Model GARCH({p},{q}) failed: {e}")
+                
+    results_df = pd.DataFrame(results)
+    results_df.sort_values(by='AIC', inplace=True)
 
-for p in p_range:
-    for q in q_range:
-        try:
-            model = arch_model(resid, vol='GARCH', p=p, q=q, dist='normal')
-            fitted_model = model.fit(disp='off')
-            results.append({'p': p, 'q': q, 'AIC': fitted_model.aic, 'BIC': fitted_model.bic})
-        except Exception as e:
-            print(f"Model GARCH({p},{q}) failed: {e}")
-            
-results_df = pd.DataFrame(results)
-results_df.sort_values(by='AIC', inplace=True)
+    best_aic_model = results_df.loc[results_df['AIC'].idxmin()]
+    best_bic_model = results_df.loc[results_df['BIC'].idxmin()]
 
-best_aic_model = results_df.loc[results_df['AIC'].idxmin()]
-best_bic_model = results_df.loc[results_df['BIC'].idxmin()]
+    print(f"Best model by AIC: GARCH({int(best_aic_model['p'])},{int(best_aic_model['q'])}) with AIC = {best_aic_model['AIC']:.4f}")
+    print(f"Best model by BIC: GARCH({int(best_bic_model['p'])},{int(best_bic_model['q'])}) with BIC = {best_bic_model['BIC']:.4f}")
 
-print(f"Best model by AIC: GARCH({int(best_aic_model['p'])},{int(best_aic_model['q'])}) with AIC = {best_aic_model['AIC']:.4f}")
-print(f"Best model by BIC: GARCH({int(best_bic_model['p'])},{int(best_bic_model['q'])}) with BIC = {best_bic_model['BIC']:.4f}")
+    best_model = arch_model(resid, vol=model_type, p=int(best_aic_model['p']), q=int(best_aic_model['q']), dist='normal', rescale=False)
+    fitted = best_model.fit(disp='off')
 
-best_model = arch_model(resid, vol='GARCH', p=int(best_aic_model['p']), q=int(best_aic_model['q']), dist='normal')
-fitted = best_model.fit(disp='off')
+    # Plot conditional volatility (standard deviation)
+    annual_vol = fitted.conditional_volatility*np.sqrt(252)
+    mod = "TGARCH" if model_type == "GARCH" and symmetry == 1 else model_type
 
-# Plot conditional volatility (standard deviation)
-annual_vol = fitted.conditional_volatility*np.sqrt(252)
-
-plt.figure(figsize=(10, 4))
-plt.plot(spx["Date"], annual_vol, label='Conditional Volatility')
-plt.title(f'GARCH({int(best_aic_model["p"])},{int(best_aic_model["q"])}) Conditional Volatility')
-plt.xlabel('Time')
-plt.ylabel('Volatility')
-#plt.ylim((0, 0.5))
-plt.legend()
-plt.tight_layout()
-plt.show()
+    plt.figure(figsize=(10, 4))
+    plt.plot(index["Date"], annual_vol, label='Conditional Volatility')
+    plt.title(f'{mod}({int(best_aic_model["p"])},{int(best_aic_model["q"])}) Conditional Volatility')
+    plt.xlabel('Time')
+    plt.ylabel('Volatility')
+    #plt.ylim((0, 0.5))
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
